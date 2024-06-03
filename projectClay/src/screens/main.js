e2c674d7f6ccec20fc15cda643ffc94e7c0056a4
Omar Almasri <o_almasri@fanshaweonline.ca>
2024-06-03 12:47:41 -0400

    Added a basic welcome screen

--- projectClay/src/screens/main.js
+++ projectClay/src/screens/main.js
@@ -1,32 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import {Text, View } from 'react-native';
-import { Canvas } from '@react-three/fiber/native';
-import styles from '../styles/styles';
-import CustomButton from '../components/CustomButton'
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Pressable,
+  CheckBox,
+} from "react-native";
+
+import {
+  useGLTF,
+  MeshTransmissionMaterial,
+  ContactShadows,
+  Environment,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Canvas } from "@react-three/fiber/native";
+import styles, { colors } from "../styles/styles";
+import CustomButton from "../components/CustomButton";
+
 export default function main() {
-
+  const [isSelected, setSelection] = useState(false);
   return (
-
     <View style={styles.container}>
-        
-    <Canvas>
-      <ambientLight intensity={0.1} />
-      <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh >
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
-    </Canvas>
-      <View style={styles.overlay}>
-          <Text style={styles.text}>2D Title</Text>
-          <View style={styles.btnContainer}>
-            <CustomButton/>
-            <CustomButton/>
-          </View>
-          
+      <Canvas style={styles.canvas} shadows>
+        <PerspectiveCamera
+          makeDefault
+          fov={50}
+          position={[0, 0, 5]} // Set the camera's position
+          rotation={[0, 0, 0]} // Set the camera's rotation
+          near={0.1} // Set the near clipping plane
+          far={1000} // Set the far clipping plane
+        />
+        <ambientLight intensity={2} />
+        <directionalLight position={[5, 10, 5]} intensity={4} castShadow />
+        <ContactShadows
+          resolution={512}
+          position={[0, -0.8, 0]}
+          opacity={0}
+          scale={100}
+          blur={0}
+          far={0.8}
+        />
+        {/* Ground Plane */}
+        <mesh
+          position={[0, -1, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          receiveShadow
+        >
+          <planeGeometry args={[10, 10]} />
+          <meshStandardMaterial color={colors.white} />
+          {/* Set the plane's color to match the background */}
+        </mesh>
+        {/* BOX */}
+        <mesh position={[-1, -0.4, 0]} rotation={[0.1, -0.4, 0]} castShadow>
+          <boxGeometry />
+          <meshStandardMaterial color={colors.orange} />
+        </mesh>
+      </Canvas>
+
+      <View style={styles.cardContainer}>
+        <View style={[styles.card, styles.shadow]}>
+          <ScrollView>
+            <View style={styles.ScrollView}>
+              <Text style={styles.title}>Welcome Back </Text>
+              <Text style={styles.subtitle}>
+                Don't have an account ? <Text>SignUp</Text>
+              </Text>
+              <View style={styles.formTextView}>
+                <Text style={styles.formText}>User Name</Text>
+              </View>
+              <TextInput style={styles.input} placeholder="" />
+              <View style={styles.formTextView}>
+                <Text style={styles.formText}>Password</Text>
+              </View>
+
+              <TextInput
+                style={styles.input}
+                placeholder="Need Better Implementation"
+              />
+              <View style={styles.checkboxContainer}>
+                <CheckBox
+                  value={isSelected}
+                  onValueChange={setSelection}
+                  style={styles.checkbox}
+                />
+                <Text style={[styles.formText, styles.nomargin]}>
+                  Remember Mes
+                </Text>
+              </View>
+
+              <Pressable style={styles.btn}>
+                <Text style={styles.buttonText}>Login</Text>
+              </Pressable>
+            </View>
+          </ScrollView>
         </View>
       </View>
-
+    </View>
   );
 }
-
--- projectClay/src/styles/styles.js
+++ projectClay/src/styles/styles.js
@@ -1,39 +1,127 @@
-import { StyleSheet } from 'react-native';
+import { Scroll } from "@react-three/drei";
+import { StyleSheet, Dimensions } from "react-native";
+import { ScrollView } from "react-native-web";
 
+const windowWidth = Dimensions.get("window").width;
+export const colors = {
+  white: "rgba(235, 235, 235, 1)", // Blue
+  black: "rgba(60, 60, 60, 1)", // White
+  pinkish: "rgba(193, 131, 159, 1)", // Orange
+  orange: "rgba(255, 90, 95, 1)", // Grey
+  teal: "rgba(8, 126, 139, 1)", // Light Grey
+
+  // ... add more colors as needed
+};
 const styles = StyleSheet.create({
-    container: { flex: 1},
-    canvas: {
-      position: 'absolute',
-      top: 0, left: 0, right: 0, bottom: 0,
-      zIndex: -1, // Place canvas behind the overlay
-      pointerEvents: 'none'
-    },
-    overlay: {
-      ...StyleSheet.absoluteFillObject,
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      margin:'20%', // margin above title and below buttons adjust later
-      
-      
-    },
-    btnContainer:{
-      //adjust later to test buttons as list items if better
-      flexDirection: "row",
-      justifyContent: "space-around",
-      flexWrap: 'wrap',
-      width:'100%'
-    },
-    btn:{
-      width:'40%'
-    },
-    text: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      color: 'black', // Adjust text color as needed
-    },
+  //entire page container
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    justifyContent: "center", //Centered vertically
+  },
+  canvas: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: -1, // Place canvas behind the overlay
+    pointerEvents: "none",
+  },
+  //Card page wide container
+  cardContainer: {
+    //backgroundColor: "rgba(125, 125, 125, 0)",
+    justifyContent: "flexStart",
+    alignItems: "center",
+    margin: "1%", // margin above title and below buttons adjust later
+    borderRadius: 10,
+    padding: 20, // Add padding
+    flexDirection: "row-reverse",
+    height: "100%",
+    paddingRight: windowWidth <= 900 ? "auto" : "10%",
+    paddingLeft: windowWidth <= 900 ? "auto" : "auto",
+  },
+  card: {
+    width: windowWidth <= 900 ? "100%" : "30%",
+    height: "50%",
+    backgroundColor: colors.white,
+    padding: "1%",
+    borderRadius: 20,
+  },
+  ScrollView: { width: "100%", alignItems: "center" },
+  input: {
+    borderColor: colors.pinkish,
+    width: "95%",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginTop: "1%",
+  },
+  title: {
+    fontSize: 32, // Adjust font size as needed
+    fontWeight: "bold",
+    color: colors.black, // White text color
+    textAlign: "center", // Center alignment
+    marginBottom: 10, // Add some space below the title
+  },
+  subtitle: {
+    fontSize: 18, // Adjust font size as needed
+    fontWeight: "bold",
+    color: colors.black, // White text color
+    textAlign: "center", // Center alignment
+    marginBottom: 10, // Add some space below the title
+  },
+  btn: {
+    backgroundColor: colors.teal, // Blue button color
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: "2%",
+    width: "95%",
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  formText: {
+    textAlign: "left",
+    color: colors.orange,
+    fontSize: 16,
+    fontWeight: "bold",
+    marginTop: "1%",
+    marginBottom: "1%",
+  },
+  formTextView: {
+    textAlign: "left",
+    width: "95%",
+    marginTop: "1%",
+  },
+  checkboxContainer: {
+    textAlign: "left",
+    flexDirection: "row",
+    width: "95%",
+    marginTop: "1%",
+  },
+  checkbox: {
+    margin: "1%",
+  },
 
-  });
+  //proper styles start here
+  shadow: {
+    shadowColor: colors.teal,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+  nomargin: {
+    marginTop: 0,
+  },
+});
 
 export default styles; // Default export
-
-
